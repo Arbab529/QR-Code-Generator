46578c3308a64720e57fb05590088df9daa614f1
Arbab <arbabanjum0347@gmail.com>
2022-06-09 21:45:32 +0500

    Events updated

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ const QRcode = document.querySelector(".qr-code"),
 generatedBtn = document.querySelector("button"),
 qrInput = document.querySelector("input"),
 qrImage = document.querySelector(".qr-code img")
- generatedBtn.addEventListener("click",(e)=>{
-     e.preventDefault();
-     let qrValue = qrInput.value;
-     if(!qrValue) return;
+
+generatedBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    generatedBtn.innerText = "Generating QR Code...";
     qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
     QRcode.classList.add("active");
+    qrImage.addEventListener("load", ()=>{
+        generatedBtn.innerText = "Generate";
+    })
+ });
+
+ qrInput.addEventListener("keyup",()=>{
+     if(!qrInput.value){
+         QRcode.classList.remove("active");
+     }
  })
